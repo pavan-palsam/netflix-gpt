650bf5b61c69a58fc984bf0b8b43576d1b50436a
CodeSandbox <support@codesandbox.io>
2024-04-25 11:21:31 +0000

    Sandbox snapshot commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="m-2 p-2 font-bold text-red-400 underline">
+        Hello CodeSandbox
+      </h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
